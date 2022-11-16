02e292404f5bd0bfd04609679fdecca1fe312a3d
kensoidev <65781959+kensoi@users.noreply.github.com>
2022-11-16 20:24:28 +0500

    Исправление под PEP8 и дописывание главной функции

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-# Выполнил Прокофьев Андрей
-# Фт-210008
+"""
+Выполнил Прокофьев Андрей
+Фт-210008
+"""
 
 import random
+
 
 COMPARE_RESULT_WORD = {
     True: "больше",
@@ -10,37 +13,56 @@ COMPARE_RESULT_WORD = {
 
 
 def get_int(text_to_print: str) -> int:
+    """
+    Получить целое число
+    """
+
     while True:
         try:
             response = int(input(text_to_print + " >> "))
+            # попытка не получить value error exception
+
+            if response < 1:
+                # если вдруг число меньше единицы, то
+                # отправляем пользователя обратно писать число
+                print("Число должно быть больше 0!")
+                continue
 
         except ValueError as value_error:
+            # если получили то отправляем пользователя обратно писать число
             print("Введите целое число! Ошибка:", value_error)
+            continue
 
-        else:
-            return response
+        return response
 
 
 def main():
     """
     Главная функция
     """
-    GUESS_VALUE_RANGE = get_int("Введите N")
-    GUESS_ATTEMPTS_COUNT = get_int("Введите k")
 
-    right_answer = random.randint(1, GUESS_VALUE_RANGE)
+    attempts_count = 1 # копия
+    guess_value_range = get_int("Введите N") # const
+    guess_attempts_count = get_int("Введите k") # const
+    right_answer = random.randint(1, guess_value_range)
 
-    while GUESS_ATTEMPTS_COUNT != 0:
-        user_answer = get_int("Введите свой ответ")
+    while attempts_count <= guess_attempts_count:
+        user_answer = get_int(f"Введите свой ответ [{attempts_count}/{guess_attempts_count}]")
 
         if user_answer == right_answer:
-            break
-        
-        response = COMPARE_RESULT_WORD[user_answer > right_answer]
-        print("Неправильно! Ваше число", response, "чем загаданное")
-        GUESS_ATTEMPTS_COUNT -= 1
+            break # если правильно, то остановить цикл.
+
+        attempts_count += 1
+        print("Неправильно! Ваше число",
+            COMPARE_RESULT_WORD[user_answer > right_answer], # больше/меньше
+            "чем загаданное.")
 
     # Проверка окончательного результата
+    if user_answer == right_answer:
+        print(f"Вы угадали с попытки №{attempts_count}!")
+
+    else:
+        print("Вы не угадали число и потратили все попытки! Загаданное число:", right_answer)
 
 
 if __name__ == "__main__":
